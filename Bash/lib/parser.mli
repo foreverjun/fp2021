1dd6fa0e611ea56106c4d37a922fe736018bf24e
TimPushkin <pushkin.td@gmail.com>
2021-10-30 19:03:08 +0300

    Remove unused code

--- Bash/lib/parser.mli
+++ Bash/lib/parser.mli
@@ -1,10 +1,1 @@
 (** Main entry of parser *)
-val parse : string -> (char Ast.t, string) result
-
-type dispatch =
-  { apps : dispatch -> char Ast.t Angstrom.t
-  ; single : dispatch -> char Ast.t Angstrom.t
-  }
-
-(* A collection of miniparsers *)
-val parse_lam : dispatch
--- Bash/lib/tests.ml
+++ Bash/lib/tests.ml
@@ -1,4 +1,1 @@
-(* ***** AUTOMATIC TESTS COULD GO HERE (JUST AN EXAMPLE) *)
-let rec fact n = if n = 1 then 1 else n * fact (n - 1)
-
-let%test _ = fact 5 = 120
+(** Automatic tests *)
